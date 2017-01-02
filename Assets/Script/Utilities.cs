5d58a1bd12d18552c688ac36d48c190af4a66c1e
Yi Wang <Noumenon@Yis-MacBook-Pro.local>
2017-01-01 21:49:08 -0700

    Coordination Transformation Function Added.

---
+++ Assets/Script/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utilities {
+
+	public static Vector2 coordinateTransform(Vector2 original) {
+		Quaternion rotation = Quaternion.Euler (0, 0, -45);
+		Vector3 tmp = new Vector3 (original.x, original.y, 0);
+		tmp = rotation * tmp;
+		return new Vector2 (tmp.x, tmp.y);
+	}
+
+	public static Vector2 coordinateReverseTransform(Vector2 original) {
+		Quaternion rotation = Quaternion.Euler (0, 0, 45);
+		Vector3 tmp = new Vector3 (original.x, original.y, 0);
+		tmp = rotation * tmp;
+		return new Vector2 (tmp.x, tmp.y);
+	}
+
+	public static Vector2 getDirection(Vector2 src, Vector2 dest) {
+		Vector2 diff = dest - src;
+		if(Mathf.Abs(diff.x) > Mathf.Abs(diff.y)) {
+			return new Vector2(1, 0);
+		} else {
+			return new Vector2(0, 1);
+		}
+	}
+}
